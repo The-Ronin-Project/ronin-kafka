50061366715cdf7e5f97003956211baee0e67b0e
Josh Smith <43484632+arssycro@users.noreply.github.com>
2023-08-03 12:13:45 -0500

    Correct idempotence config (#51)

--- src/main/kotlin/com/projectronin/kafka/config/RoninProducerKafkaProperties.kt
+++ src/main/kotlin/com/projectronin/kafka/config/RoninProducerKafkaProperties.kt
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.producer.ProducerConfig.ACKS_CONFIG
 import org.apache.kafka.clients.producer.ProducerConfig.BATCH_SIZE_CONFIG
 import org.apache.kafka.clients.producer.ProducerConfig.BUFFER_MEMORY_CONFIG
 import org.apache.kafka.clients.producer.ProducerConfig.COMPRESSION_TYPE_CONFIG
+import org.apache.kafka.clients.producer.ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG
 import org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG
 import org.apache.kafka.clients.producer.ProducerConfig.LINGER_MS_CONFIG
 import org.apache.kafka.clients.producer.ProducerConfig.MAX_BLOCK_MS_CONFIG
@@ -39,7 +40,7 @@ class RoninProducerKafkaProperties(vararg configs: Pair<String, *>) {
                 put("$VALUE_SERIALIZER_CLASS_CONFIG.encoding", StandardCharsets.UTF_8.name())
                 put(VALUE_SERIALIZER_CLASS_CONFIG, ByteArraySerializer::class.qualifiedName)
                 put(ACKS_CONFIG, "all") // kafka default of all
-                put("enable.idempotent", false)
+                put(ENABLE_IDEMPOTENCE_CONFIG, false)
                 put(RETRIES_CONFIG, 3)
                 put(MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, 1) // kafka default is 5
                 put(BUFFER_MEMORY_CONFIG, 32L * MB) // kafka default of 32MB
